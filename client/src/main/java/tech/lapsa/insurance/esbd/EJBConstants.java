a9c5e1f0bd180c2d44720de49859725466fe3790
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-12-25 16:05:07 +0600

    constants

--- client/src/main/java/tech/lapsa/insurance/esbd/EJBConstants.java
+++ client/src/main/java/tech/lapsa/insurance/esbd/EJBConstants.java
@@ -2,6 +2,6 @@ package tech.lapsa.insurance.esbd;
 
 public interface EJBConstants {
 
-    public static final String APPLICATION_NAME = "insurance-esbd-app";
-    public static final String MODULE_NAME = "insurance-esbd";
+    public static final String APPLICATION_NAME = "esbd-dao-app";
+    public static final String MODULE_NAME = "esbd-dao";
 }
