19bdb778dd5f47d32f6941afd57fc99f83376eb4
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-01-08 09:43:57 +0600

    LOCAL & REMOTE

--- client/src/main/java/tech/lapsa/esbd/dao/GeneralService.java
+++ client/src/main/java/tech/lapsa/esbd/dao/GeneralService.java
@@ -4,5 +4,11 @@ import tech.lapsa.java.commons.exceptions.IllegalArgument;
 
 public interface GeneralService<T, I> extends EJBConstants {
 
+    public interface GeneralServiceLocal<T, I> extends GeneralService<T, I> {
+    }
+
+    public interface GeneralServiceRemote<T, I> extends GeneralService<T, I> {
+    }
+
     T getById(I id) throws IllegalArgument, NotFound;
 }
