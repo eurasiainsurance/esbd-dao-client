fed04ad7150eea5ede891c4bed18a3a56766dd36
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-01-09 11:30:40 +0600

    TransactionAttribute

--- beans/src/main/java/tech/lapsa/esbd/dao/PingServiceBean.java
+++ beans/src/main/java/tech/lapsa/esbd/dao/PingServiceBean.java
@@ -2,6 +2,8 @@ package tech.lapsa.esbd.dao;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 
 import tech.lapsa.esbd.connection.Connection;
 import tech.lapsa.esbd.connection.ConnectionException;
@@ -20,6 +22,7 @@ public class PingServiceBean implements PingServiceLocal, PingServiceRemote {
     private ConnectionPool pool;
 
     @Override
+    @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
     public void ping() throws IllegalState {
 	try (Connection con = pool.getConnection()) {
 	} catch (ConnectionException ce) {
